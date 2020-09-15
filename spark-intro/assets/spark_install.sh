c3315f2f2f41b6c9324ef7162f2b36043e34e94a
Sergio Nigro <sergio.nigro@enel.com>
2020-09-15 17:37:01 +0200

    Updated spark version (2.4.7)

--- spark-intro/assets/spark_install.sh
+++ spark-intro/assets/spark_install.sh
@@ -4,9 +4,9 @@ sudo apt-get install -y openjdk-8-jdk
 sudo update-java-alternatives -s java-1.8.0-openjdk-amd64
 
 echo 'Installing Spark...'
-wget https://downloads.apache.org/spark/spark-2.4.6/spark-2.4.6-bin-hadoop2.7.tgz
-tar -xvf spark-2.4.6-bin-hadoop2.7.tgz
-sudo mv spark-2.4.6-bin-hadoop2.7/ /opt/spark
+wget https://downloads.apache.org/spark/spark-2.4.7/spark-2.4.7-bin-hadoop2.7.tgz
+tar -xvf spark-2.4.7-bin-hadoop2.7.tgz
+sudo mv spark-2.4.7-bin-hadoop2.7/ /opt/spark
 
 echo "export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64/" >> ~/.profile
 echo "export SPARK_HOME=/opt/spark" >> ~/.profile
